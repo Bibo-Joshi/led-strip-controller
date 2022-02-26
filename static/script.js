f924920f8e462b64354858aafa3a3f8e62e9647d
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2022-02-26 09:06:44 +0100

    Play around with display properties a bit

--- static/script.js
+++ static/script.js
@@ -4,6 +4,34 @@ const baseUrl = window.location.protocol + '//' + window.location.host
 const apiUrl = baseUrl + '/api';
 const WSocket = new WebSocket('ws://' + window.location.host + '/ws');
 const powerToggle = document.getElementById('power-toggle');
+const backgroundElement = document.getElementById('body');
+
+function rgbAverage(color1, color2) {
+    return {
+        r: (color1.red + color2.red) / 2,
+        g: (color1.green + color2.green) / 2,
+        b: (color1.blue + color2.blue) / 2
+    };
+}
+
+function rgbToHex(rgb) {
+    return "#" + ((1 << 24) + (rgb.r << 16) + (rgb.g << 8) + rgb.b).toString(16).slice(1);
+}
+
+function updateBackgroundColor(color, other_picker) {
+    let newRGB;
+    if (other_picker.color.value === 0) {
+        newRGB = color.rgb;
+    } else {
+        if (color.value > 0) {
+            newRGB = rgbAverage(color, other_picker.color);
+        } else {
+            newRGB = other_picker.color.rgb;
+        }
+    }
+
+    backgroundElement.style.backgroundColor = rgbToHex(newRGB);
+}
 
 const RGBPicker = new iro.ColorPicker(".RGBPicker", {
     // color picker options
@@ -93,6 +121,20 @@ RGBPicker.on(["color:init", "color:change"], function (color) {
         "hsl: " + color.hslString].join("<br>");
 
     hexInput.value = color.hexString;
+
+    updateBackgroundColor(color, WWPicker);
+    // if (WWPicker.color.value === 0) {
+    //     backgroundElement.style.backgroundColor = color.hexString;
+    // } else {
+    //     if (color.value > 0) {
+    //         backgroundElement.style.backgroundColor = colorAverage(color, WWPicker.color);
+    //     } else {
+    //         backgroundElement.style.backgroundColor = WWPicker.color.hexString;
+    //     }
+    // }
+});
+WWPicker.on("color:change", function (color) {
+    updateBackgroundColor(color, RGBPicker);
 });
 
 RGBPicker.on('input:change', function (color) {
@@ -100,7 +142,13 @@ RGBPicker.on('input:change', function (color) {
         alert('No connection to server. Color change will not propagate.');
         return;
     }
-    WSocket.send(JSON.stringify({updateRGB: {red: color.red, green: color.green, blue: color.blue}}));
+    WSocket.send(JSON.stringify({
+        updateRGB: {
+            red: color.red,
+            green: color.green,
+            blue: color.blue
+        }
+    }));
 });
 WWPicker.on('input:change', function (color) {
     if (WSocket.readyState !== WSocket.OPEN) {
